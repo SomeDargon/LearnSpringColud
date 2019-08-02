3ada8e74b82dd5d72623216551b1287005c9f71d
Somedargon <296569845@qq.com>
2019-08-02 18:37:21 +0800

    1.  微服务学习提交

---
+++ study-micro-service/serice-ribbon-two/src/main/java/com/study/ribbon/RibbonTwoApplication.java
@@ -0,0 +1,28 @@
+package com.study.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableHystrix
+@SpringBootApplication
+public class RibbonTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( RibbonTwoApplication.class, args );
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
